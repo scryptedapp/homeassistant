48da30872bce7e539c826c5eb040a855274cee26
Jacob McSwain <jacob.a.mcswain@gmail.com>
2024-10-24 18:52:21 -0500

    chore(notify): adapt for proposed SDK addition

--- src/notify.ts
+++ src/notify.ts
@@ -13,7 +13,7 @@ export class NotifyDevice extends ScryptedDeviceBase implements Notifier {
         let image: string;
         let data: any = {
             priority: 'high',
-            channel: 'scrypted',
+            channel: 'scrypted'
         };
 
         if (typeof media === 'string') {
@@ -56,6 +56,10 @@ export class NotifyDevice extends ScryptedDeviceBase implements Notifier {
 
         if (image)
             data.image = image;
+
+         // Append the channel to the default 'scrypted' channel, if specified
+        if (options?.channel && options?.channel.length > 0)
+            data.channel += "_" + options.channel
 
         if (options?.data?.ha)
             Object.assign(data, options.data.ha)
